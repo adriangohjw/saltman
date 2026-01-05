4270cf7b1769109da9a5bbcaef01a98fbe6b4d87
adriangohjw <adriangohjw@gmail.com>
2026-01-05 17:40:45 +0800

    test: add vulnerable code

---
+++ examples/views/search.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+interface SearchResultsProps {
+  searchQuery: string;
+}
+
+// VULNERABLE: Reflected XSS - user input rendered without sanitization
+export function SearchResults({ searchQuery }: SearchResultsProps) {
+  return (
+    <div>
+      <h1>Search Results</h1>
+      {/* Dangerous: Directly rendering user input */}
+      <div dangerouslySetInnerHTML={{ __html: `Search results for: ${searchQuery}` }} />
+
+      {/* Also vulnerable: Direct interpolation in JSX */}
+      <p>You searched for: {searchQuery}</p>
+    </div>
+  );
+}
