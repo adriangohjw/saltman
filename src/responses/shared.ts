29638fc2f30c7609770bc00267cfdb492c73ccb9
adriangohjw <adriangohjw@gmail.com>
2026-01-06 17:13:03 +0800

    feat: enhance getSaltmanFooter to conditionally include commit link based on presence of commitSha

--- src/responses/shared.ts
+++ src/responses/shared.ts
@@ -1,17 +1,26 @@
+const SALTMAN_REPO_URL = "https://github.com/adriangohjw/saltman";
+const SHORT_SHA_LENGTH = 7;
+
 interface GetSaltmanFooterProps {
   owner: string;
   repo: string;
-  commitSha: string;
+  commitSha?: string;
 }
 
 export const getSaltmanFooter = ({ owner, repo, commitSha }: GetSaltmanFooterProps): string => {
-  const saltmanLink = `[Saltman](https://github.com/adriangohjw/saltman)`;
-  const shortSha = commitSha.substring(0, 7);
-  const commitUrl = `https://github.com/${owner}/${repo}/commit/${commitSha}`;
-  const commitLink = `[${shortSha}](${commitUrl})`;
-  return `<sub>
+  const saltmanLink = `[Saltman](${SALTMAN_REPO_URL})`;
+  const footerBase = `<sub>
 
 ---
 
-Written by ${saltmanLink} for commit ${commitLink}.</sub>`;
+Written by ${saltmanLink}`;
+
+  if (commitSha) {
+    const shortSha = commitSha.substring(0, SHORT_SHA_LENGTH);
+    const commitUrl = `https://github.com/${owner}/${repo}/commit/${commitSha}`;
+    const commitLink = `[${shortSha}](${commitUrl})`;
+    return `${footerBase} for commit ${commitLink}.</sub>`;
+  }
+
+  return `${footerBase}.</sub>`;
 };
